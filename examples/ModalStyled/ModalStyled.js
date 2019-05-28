cac5a32e4c3cb5f7b6a4b5537d66450a10191639
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-05-28 14:40:42 +0300

    [elements] Add ModalBasic

---
+++ examples/ModalStyled/ModalStyled.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import { ModalBasic } from '../../src';
+
+const ModalStyled = ({
+  id,
+  modalHandlerProps,
+  children,
+  className,
+}) => (
+  <ModalBasic
+    id={id}
+    modalHandlerProps={modalHandlerProps}
+    className={cn('hrf-modal-styled', className)}
+  >
+    <button
+      type="button"
+      onClick={ModalBasic.Actions.initModalByIdToggler(id, modalHandlerProps)}
+    >
+      Close
+    </button>
+    {children}
+  </ModalBasic>
+);
+
+ModalStyled.defaultProps = {
+  className: '',
+  modalHandlerProps: {},
+};
+
+ModalStyled.propTypes = {
+  id: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+
+  className: PropTypes.string,
+  modalHandlerProps: PropTypes.shape({
+    onClose: PropTypes.func,
+  }),
+};
+
+export default ModalStyled;
