6b469338451dec5fcc6706601ab6691c8c218b97
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-07-04 15:56:33 +0300

    [refactoring] modal basic: use overlay hoc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // ui-elements
-export { ModalProvider, ModalBasic } from './ModalBasic';
+export { ModalBasic } from './ModalBasic';
 export { Container, Row, Col } from './Layout';
 
 // hocs
 export {
   OverlayProvider,
+
   createOverlayBasicComponent,
   initOverlayByIdToggler,
 } from './hocs';
