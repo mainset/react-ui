85462046480cf40b5928934fbe0fa16cc8081bcc
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2020-06-22 22:37:50 +0300

    feat: create text tag

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { Container, Row, Col } from './Layout';
 export { ModalBasic } from './ModalBasic';
 export { Popover } from './Popover';
 export { SidebarBasic } from './SidebarBasic';
+export { TextTag } from './TextTag';
 // export { Heading, Paragraph, TextSection } from './Typography';
 
 // hocs
